1603592033bddad60a3f769508c54540a0fdacb0
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-16 10:08:44 -0500

    adding based prj

---
+++ src/main/java/com/signallin/signall_app/SignallAppApplication.java
@@ -0,0 +1,13 @@
+package com.signallin.signall_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignallAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignallAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signallin/signall_app/SignallAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signallin.signall_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignallAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
